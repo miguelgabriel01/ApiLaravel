a7b805f00da07de7d9c64cad91e56cf695ffaf31
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-10-24 17:50:27 -0300

    Alteração na seeder de usuarios

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -22,6 +22,46 @@ class UsersTableSeeder extends Seeder
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s"),
             ],
+            1 => [
+                'name' => 'Gabriel Barbosa D. Santos',
+                'email' => 'gabriel@gmail.com',
+                'password' => bcrypt('miguel123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+            2 => [
+                'name' => 'Miguel B. Dos Santos',
+                'email' => 'miguel@gmail.com',
+                'password' => bcrypt('miguel123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+
+            3 => [
+                'name' => 'João Gabriel  Dos Santos',
+                'email' => 'joaogabriel@gmail.com',
+                'password' => bcrypt('miguel123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+
+            4 => [
+                'name' => 'Miguel Israel B. Dos Santos',
+                'email' => 'miguelisrael@gmail.com',
+                'password' => bcrypt('miguel123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+
+            5 => [
+                'name' => 'Ryan Hudson Dos Santos',
+                'email' => 'hudson@gmail.com',
+                'password' => bcrypt('miguel123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+
+
         ];
 
         User::insert($users);
