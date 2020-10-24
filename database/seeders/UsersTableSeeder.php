0eb14cca4f3cf0cd678da75b27af356e8ed397fb
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-10-24 17:29:44 -0300

    Criação de seeders de usuarios

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            0 => [
+                'name' => 'Miguel Gabriel B. Dos Santos',
+                'email' => 'gabrielogabriel10@gmail.com',
+                'password' => bcrypt('miguel123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
