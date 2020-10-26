45a4101d24c0c8c84c0979d30a0572f7f60de7b5
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-10-26 11:00:53 -0300

    Edição do middleware de autenticação

--- app/Http/Middleware/apiProtectedRoute.php
+++ app/Http/Middleware/apiProtectedRoute.php
@@ -4,8 +4,10 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\http\Middleware\BaseMiddleware;
+use Tymon\JWTAuth\JWTAuth;
 
-class apiProtectedRoute
+class apiProtectedRoute extends BaseMiddleware
 {
     /**
      * Handle an incoming request.
@@ -16,6 +18,18 @@ class apiProtectedRoute
      */
     public function handle(Request $request, Closure $next)
     {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (\Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status' => 'Token is Invalid']);
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status' => 'Token is Expired']);
+            }else{
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
         return $next($request);
+    
     }
 }
